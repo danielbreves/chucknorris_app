009d5a76f9e261fc5bc268a8c41a10fbe914fd95
Jeremiah Currier <jcurrier@zendesk.com>
2014-05-12 16:12:58 -0700

    adding error handling to app.js file

--- app.js
+++ app.js
@@ -1,71 +1,65 @@
 (function () {
-	return {
-		requests: {
-			fetchNorris: function () {
-				var currentUser = this.currentUser();
-				return {
-					url: 'http://api.icndb.com/jokes/random?escape=javascript',
-					type: 'GET',
-					data: {
-						firstName: currentUser.name,
-						lastName: ""
-					}
-				};
-			},
+    return {
+        requests: {
+            fetchNorris: function () {
+                var currentUser = this.currentUser();
+                return {
+                    url: 'http://api.icndb.com/jokes/random?escape=javascript',
+                    type: 'GET',
+                    data: {
+                        firstName: currentUser.name,
+                        lastName: ""
+                    }
+                };
+            },
 
-			fetchNorrisFiltered: function () {
-				var currentUser = this.currentUser();
-				return {
-					url: 'http://api.icndb.com/jokes/random?limitTo=[nerdy]',
-					type: 'GET',
-					data: {
-						firstName: currentUser.name,
-						lastName: ""
-					}
-				};
-			}
-		},
-		events: {
-			'app.activated': 'init',
-			'fetchNorris.done': function (data) {
-				this.renderNorris(data.value.joke);
-			},
-			'fetchNorris.fail': function (data){
-				this.errorNotify('fetchNorris');
-			},
-			'fetchNorrisFiltered.done': function (data) {
-				this.renderNorris(data.value.joke);
-			},
-			'fetchNorrisFiltered.fail': function (data){
-				this.errorNotify('fetchNorrisFiltered');
-			}
-		},
+            fetchNorrisFiltered: function () {
+                var currentUser = this.currentUser();
+                return {
+                    url: 'http://api.icndb.com/jokes/random?limitTo=[nerdy]',
+                    type: 'GET',
+                    data: {
+                        firstName: currentUser.name,
+                        lastName: ""
+                    }
+                };
+            }
+        },
+        events: {
+            'app.activated': 'init',
+            'fetchNorris.done': function (data) {
+                this.renderNorris(data.value.joke);
+            },
+            'fetchNorris.fail': function (data){
+                this.ajax('fetchNorris'); // Retrying request
+            },
+            'fetchNorrisFiltered.done': function (data) {
+                this.renderNorris(data.value.joke);
+            },
+            'fetchNorrisFiltered.fail': function (data){
+                this.ajax('fetchNorrisFiltered'); // Retrying request
+            }
+        },
 
-		errorNotify: function(request) {
-			var self=this;
-			services.notify("There's an issue getting the quotes but check this out: <a href='http://bit.ly/1kuKXqv'>Chuck Norris in a dress.</a>", "alert");
-			setTimeout(function(){ self.ajax(request);}, 500);
-		},
-
-		init: function () {
-			if ( this.setting('partyPooper') === false ) {
-				this.ajax('fetchNorris');
-				this.counter = 1;
-			} else {
-				this.ajax('fetchNorrisFiltered');
-				this.counter = 1;
-			}
-		},
-		renderNorris: function (fact) {
-			var image = 'norris-' + this.counter + '.jpg';
-			this.counter = Math.max(1,((this.counter + 1) % 4));
-			var self = this;
-			this.switchTo('layout', {
-				fact: fact,
-				image: image
-			});
-			setTimeout(function(){ self.ajax('fetchNorris'); }, 5000);
-		}
-	};
+        init: function () {
+            if ( this.setting('partyPooper') === false ) {
+                this.ajax('fetchNorris');
+                this.counter = 1;
+            } else {
+                this.ajax('fetchNorrisFiltered');
+                this.counter = 1;
+            }
+        },
+        renderNorris: function (fact) {
+            var image = 'norris-' + this.counter + '.jpg';
+            this.counter = Math.max(1,((this.counter + 1) % 4));
+            var self = this;
+            this.switchTo('layout', {
+                fact: fact,
+                image: image
+            });
+            setTimeout(function(){ self.ajax('fetchNorris'); }, 5000);
+        }
+    };
 
 }());
